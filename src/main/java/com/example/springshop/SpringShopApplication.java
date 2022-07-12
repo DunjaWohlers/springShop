636c0e1bddc5a5a6f5cbbeb21f14c3a0d7c74a51
dunjawohlers <dunjawohlers@gmx.de>
2022-07-12 16:17:23 +0200

    post added

--- src/main/java/com/example/springshop/SpringShopApplication.java
+++ src/main/java/com/example/springshop/SpringShopApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShopApplication.class, args);
+    }
 
 }
